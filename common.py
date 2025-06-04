c7707045c165dc87d350c858703b0c0fffbabab4
303jae <jyunchoi0710@naver.com>
2025-06-05 03:21:01 +0900

    Delete common.py

--- common.py
+++
@@ -1,9 +0,0 @@
-
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-def load_dataset(region="E", with_cluster=True):
-    base = f"data/{region}/"
-    filename = f"preprocessed_with_cluster_{region}.csv" if with_cluster else f"preprocessed_{region}.csv"
-    df = pd.read_csv(base + filename)
-    return df.dropna(subset=["DGSTFN"])
